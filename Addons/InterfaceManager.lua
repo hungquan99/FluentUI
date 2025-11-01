15f0367872d73c55872cd7f856d72684112ffe14
hungquan99 <142434237+hungquan99@users.noreply.github.com>
2025-11-01 18:58:53 +0700

    Update InterfaceManager.lua

--- Addons/InterfaceManager.lua
+++ Addons/InterfaceManager.lua
@@ -61,6 +61,29 @@ local InterfaceManager = {} do
         local Settings = InterfaceManager.Settings
 
         InterfaceManager:LoadSettings()
+
+		local section = tab:AddSection("Interface")
+
+		local InterfaceTheme = section:AddDropdown("InterfaceTheme", {
+			Title = "Theme",
+			Description = "Changes the interface theme.",
+			Values = Library.Themes,
+			Default = Settings.Theme,
+			Callback = function(Value)
+				Library:SetTheme(Value)
+                Settings.Theme = Value
+                InterfaceManager:SaveSettings()
+			end
+		})
+
+        InterfaceTheme:SetValue(Settings.Theme)
+	
+		local MenuKeybind = section:AddKeybind("MenuKeybind", { Title = "Minimize Bind", Default = Settings.MenuKeybind })
+		MenuKeybind:OnChanged(function()
+			Settings.MenuKeybind = MenuKeybind.Value
+            InterfaceManager:SaveSettings()
+		end)
+		Library.MinimizeKeybind = MenuKeybind
     end
 end
 
